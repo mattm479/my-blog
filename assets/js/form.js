3f465830d12dd38de0e0e734f3047a3a0c131590
Matthew Miskov <mattm479@gmail.com>
2024-03-21 18:29:14 -0400

    Creating form and storing form data in localStorage

---
+++ assets/js/form.js
@@ -0,0 +1,19 @@
+document.getElementById("blog-entry-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log(event);
+    const username = event.target.elements.username.value;
+    const title = event.target.elements.title.value;
+    const content = event.target.elements.content.value;
+    const blogEntry = {
+        "username": username,
+        "title": title,
+        "content": content
+    };
+
+    let currentEntries = JSON.parse(localStorage.getItem("blog-entries"));
+    if (currentEntries === null) localStorage.setItem("blog-entries", JSON.stringify([ blogEntry ]));
+    else {
+        currentEntries.unshift(blogEntry);
+        localStorage.setItem("blog-entries", JSON.stringify(currentEntries));
+    }
+});
